42e3e363eddd84431a0c36fa516db9249b9cc2c5
smint86 <smint@rocketmap.do>
2017-08-03 02:56:28 +0200

    fixed PA bash

--- bashs/restartdiscord.sh
+++ bashs/restartdiscord.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd
 screen -X -S DiscordAlarm quit
-/usr/bin/screen -AmdS DiscordAlarm /opt/python/bin/python /root/PokeAlarm/start_pokealarm.py -cf /root/PokeAlarm/config/config_new.ini
+/usr/bin/screen -AmdS DiscordAlarm /opt/python/bin/python /root/PokeAlarm/start_pokealarm.py -cf /root/PokeAlarm/config/config.ini
 sleep 2
