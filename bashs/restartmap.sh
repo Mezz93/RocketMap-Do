f548327dd36b409ef657e27874db90735fd22799
smint86 <smint@rocketmap.do>
2017-08-10 20:16:57 +0200

    Updated file list

--- bashs/restartmap.sh
+++ bashs/restartmap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd
 screen -X -S DoMap quit
-cd /root/RocketMap/
-/usr/bin/screen -AmdS DoMap /opt/python/bin/python /root/RocketMap/runserver.py -novc -os -sn DortmundMap -l "51.5131377,7.4647469" -cf /root/RocketMap/config/config.ini -pd 168
+cd /root/RocketMap-Do-Dev/
+/usr/bin/screen -AmdS DoMap /opt/python/bin/python /root/RocketMap-Do-Dev/runserver.py -novc -os -sn DortmundMap -l "51.5131377,7.4647469" -cf /root/RMDocs/config/config.ini -pd 168
 sleep 2
